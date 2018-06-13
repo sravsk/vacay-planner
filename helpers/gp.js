28a195bb564424b0afd3938dd731eece6fee4e19
Neiman Chen <hahatuke@gmail.com>
2018-06-12 20:09:19 -0700

    switched lat and lng

--- helpers/gp.js
+++ helpers/gp.js
@@ -4,8 +4,8 @@ const request = require('request');
 const key = config.GP_KEY;
 
 module.exports = {
-  getPOI: (lng, lat, callback) => {
-    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lng},${lat}&radius=1600&key=${key}`;
+  getPOI: (lat, lng, callback) => {
+    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=1600&key=${key}`;
     request.get(url, (err, res, body) => {
       err ? console.log(err) : callback(body);
     })
