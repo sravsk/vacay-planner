51842f4d8950678d6cc0688c6b1afb613999f83d
sravsk <sravanthi.karanam@gmail.com>
2018-06-13 16:16:19 -0700

    code cleanup

--- database/index.js
+++ database/index.js
@@ -237,7 +237,6 @@ var dbHelpers = {
 
   //this will delete a trip by id and pass the remaining trips to server
   deleteTripID: (tripId, cb) => {
-    console.log("trip id", tripId)
     Trip.findOne({where: {id: tripId}}).then(trip => {
       return  trip.destroy({
         where : {
