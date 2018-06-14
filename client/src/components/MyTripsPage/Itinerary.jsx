22079bde9e7211369baf9128eac06cbaf3864e5e
Amena <syedaamenakauser@gmail.com>
2018-06-14 17:08:29 -0400

    Refactor

--- client/src/components/MyTripsPage/Itinerary.jsx
+++ client/src/components/MyTripsPage/Itinerary.jsx
@@ -3,22 +3,31 @@ import React from 'react';
 class Itinerary extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      onAddRestToItinerary: false
+    }
+    this.addRestaurant = this.addRestaurant.bind(this);
   }
+
+  addRestaurant (index, date) { // reference day by index i
+    console.log('In addRestaurant function', index, date)
+  }
+
 
   render () {
     let toRender = [];
     let itinerary = this.props.itinerary;
-    for (var i = 0; i < itinerary.length; i++) {
-      var date = Object.keys(itinerary[i])[0];
-      toRender[i] = (
-        <div className="day" key={i}>
+    itinerary.forEach((day, index) => {
+      var date = Object.keys(day)[0];
+      toRender[index] = (
+        <div className="day" key={index}>
           {date}
           <br/><br/>
-          <button>Add restaurant</button><br/><br/>
+          <button onClick={() => this.addRestaurant(index, date)}>Add restaurant</button><br/><br/>
           <button>Add point of interest</button>
         </div>
       );
-    }
+    })
     return (
       <div className="wrapper">
         {toRender}
