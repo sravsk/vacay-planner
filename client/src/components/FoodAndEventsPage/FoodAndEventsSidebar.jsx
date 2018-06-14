4e14ccd3e4b58dad4c8d123f8751fecacb09c32c
Amena <syedaamenakauser@gmail.com>
2018-06-14 01:16:10 -0400

    Refactor FoodandEventsSidebar to a stateless component

--- client/src/components/FoodAndEventsPage/FoodAndEventsSidebar.jsx
+++ client/src/components/FoodAndEventsPage/FoodAndEventsSidebar.jsx
@@ -6,54 +6,35 @@ import axios from 'axios';
 import FavFoodCard from './FavFoodCard.jsx';
 import FavEventCard from './FavEventCard.jsx';
 
-class FoodAndEventsSidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: undefined
-    }
-  }
-
-  componentDidMount() {
-    axios.get('/user')
-      .then(result => {
-        this.setState({
-          user: result.data
-        });
-        console.log('user', result.data)
-      })
-  }
-
-  render () {
-    console.log('user', this.state.user)
-    const panes = [
+const FoodAndEventsSidebar = (props) => {
+  const panes = [
     {
       menuItem: 'Favorites', render: () => (
         <Tab.Pane>
-          <Input focus placeholder="Enter your trip name" value={this.props.tripName} onChange={this.props.onNameChange} /><br /><br />
-          {this.props.foodFavorites.length > 0 ? <h2>Restaurants</h2> : ''}
+          <Input focus placeholder="Enter your trip name" value={props.tripName} onChange={props.onNameChange} /><br /><br />
+          {props.foodFavorites.length > 0 ? <h2>Restaurants</h2> : ''}
           <List celled size='large'>
-            {this.props.foodFavorites.map((restaurant, index) => {
+            {props.foodFavorites.map((restaurant, index) => {
               return <FavFoodCard restaurant={restaurant} key={'favfood' + restaurant.id} />;
             })}
           </List>
-          {this.props.eventFavorites.length > 0 ? <h2>Events</h2> : ''}
+          {props.eventFavorites.length > 0 ? <h2>Events</h2> : ''}
           <List celled size='large'>
-            {this.props.eventFavorites.map((event, index) => {
+            {props.eventFavorites.map((event, index) => {
                 return <FavEventCard event={event} key={'favevent' + event.id} />;
             })}
           </List>
-          {this.props.foodFavorites.length + this.props.eventFavorites.length === 0 ? '' : (
+          {props.foodFavorites.length + props.eventFavorites.length === 0 ? '' : (
             <div>
               {
-                !(this.state.user) ?
+                !(props.user) ?
                 (<div>
-                  <Button onClick={() => {this.props.history.push('/login')}} color='blue' fluid content='Login to Save' />
+                  <Button onClick={() => {props.history.push('/login')}} color='blue' fluid content='Login to Save' />
                   <hr />
-                  <Button onClick={() => {this.props.history.push('/signup')}} fluid content='Not a member? Sign Up' />
+                  <Button onClick={() => {props.history.push('/signup')}} fluid content='Not a member? Sign Up' />
                 </div>)
                 :
-                <Button onClick={this.props.saveTrip} content='Save to My Trips' fluid />
+                <Button onClick={props.saveTrip} content='Save to My Trips' fluid />
 
               }
             </div>
@@ -63,7 +44,6 @@ class FoodAndEventsSidebar extends React.Component {
     }
   ];
   return <Tab panes={panes} />;
-  }
 }
 
 export default withRouter(FoodAndEventsSidebar);
